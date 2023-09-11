9e356a8aa22bc289b97c1381795414504aadd746
ChenXr <warrior22@qq.com>
2023-09-11 01:32:44 +0000

    fix:修复带date的多字段插入bug

--- src/observer/sql/stmt/insert_stmt.cpp
+++ src/observer/sql/stmt/insert_stmt.cpp
@@ -72,8 +72,8 @@ RC InsertStmt::create(Db* db, const InsertSqlNode& inserts, Stmt*& stmt) {
                 return RC::SCHEMA_FIELD_TYPE_MISMATCH;
             }
         }
-        // everything alright
-        stmt = new InsertStmt(table, values, value_num);
-        return RC::SUCCESS;
     }
+    // everything alright
+    stmt = new InsertStmt(table, values, value_num);
+    return RC::SUCCESS;
 }
